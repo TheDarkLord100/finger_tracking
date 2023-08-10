f09db3e8c59a3ca8feac6f9abaf1a0e0e83a33b1
TheDarkLord100 <miracleyt404@gmail.com>
2023-08-10 13:38:39 +0530

    Restricted to single hand

--- FingerDetection.py
+++ FingerDetection.py
@@ -28,28 +28,29 @@ print(len(overlayList))
 
 while True:
     success, img = cap.read()
-    img = detector.findHands(img=img)
+    img, singleHand = detector.findHands(img=img)
     lmList = detector.findPosition(img=img, draw=False)
 
-    if len(lmList) != 0:
-        fingers = []
-        if (lmList[4][1] < lmList[3][1]):
-            fingers.append(1)
-        else:
-            fingers.append(0)
-        for id in range(0, 4):
-            # print(lmList[4][1])
-            # print(lmList[2][1])
-            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+    if singleHand:
+        if len(lmList) != 0:
+            fingers = []
+            if (lmList[4][1] < lmList[3][1]):
                 fingers.append(1)
             else:
                 fingers.append(0)
+            for id in range(0, 4):
+                # print(lmList[4][1])
+                # print(lmList[2][1])
+                if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
 
-        print(fingers)
-        totalFingers = fingers.count(1)
+            print(fingers)
+            totalFingers = fingers.count(1)
 
-        h, w, c = overlayList[totalFingers].shape
-        img[0:h, 0:w] = overlayList[totalFingers]
+            h, w, c = overlayList[totalFingers].shape
+            img[0:h, 0:w] = overlayList[totalFingers]
 
 
 
--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -20,13 +20,17 @@ class handDetector():
 
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-
+        singleHand = True
         if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:
-                if draw:
-                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+            if(len(self.results.multi_handedness) == 2):
+                cv2.putText(img, "Use ONE HAND", (200, 400), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+                singleHand = False
+            else:
+                for handLms in self.results.multi_hand_landmarks:
+                    if draw:
+                        self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
 
-        return img
+        return img, singleHand
 
     def findPosition(self, img, handNo=0, draw=True):
 
